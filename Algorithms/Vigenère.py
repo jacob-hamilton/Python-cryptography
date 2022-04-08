f0f7540c2b8feb3c67b9e9ec6bb4e683fa09f066
jacob2004hamilton <92182228+jacob2004hamilton@users.noreply.github.com>
2022-04-08 12:49:18 +0100

    Corrected Vigenère Cipher.

--- Algorithms/Vigenère.py
+++ Algorithms/Vigenère.py
@@ -1,45 +1,19 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 def vigenereCipher(plainText, key):
-    key = paddKey(plainText, key.lower())
-    plainText = plainText.lower()
-    result = ""
-    for i in range(0, len(plainText)):
-        result += vignereTable(plainText[i], key[i])
-    return result
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    plaintext_int = [ord(i) for i in plainText]
+    ciphertext = ''
+    for i in range(len(plaintext_int)):
+        value = (plaintext_int[i] + key_as_int[i % key_length]) % 26
+        ciphertext += chr(value + 65)
+    return ciphertext
 
-def inverseVigenereCipher(cipherText, key):
-    key = paddKey(cipherText, key.lower())
-    result = ""
-    for i in range(0, len(cipherText)):
-        result += inverseVignereTable(key[i], cipherText[i])
-    return result
-
-def vignereTable(rowLetter, columnLetter):
-    temp = alphabet
-    for i in range(alphabet.index(rowLetter)):
-        temp.append(temp[0])
-        temp = temp[1:]
-    return temp[alphabet.index(columnLetter)]
-
-def inverseVignereTable(keyLetter, cipherLetter):
-    temp = alphabet
-    for i in range(alphabet.index(keyLetter)):
-        temp.append(temp[0])
-        temp = temp[1:]
-    return alphabet[temp.index(cipherLetter)]
-
-def paddKey(plainText, key):
-    plainTextLength = len(plainText)
-    count = 0
-
-    if len(key) < plainTextLength:
-        while len(key) < plainTextLength:
-            key += key[count]
-            count += 1
-        return key
-
-    if len(key) > plainTextLength:
-        return key[0:plainTextLength]
-    
-    return key
+def inverseVigenerCipher(cipherText, key):
+    key_length = len(key)
+    key_as_int = [ord(i) for i in key]
+    ciphertext_int = [ord(i) for i in cipherText]
+    plaintext = ''
+    for i in range(len(ciphertext_int)):
+        value = (ciphertext_int[i] - key_as_int[i % key_length]) % 26
+        plaintext += chr(value + 65)
+    return plaintext
